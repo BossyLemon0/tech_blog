b1e41e02257058d5b2d3bb36b66ab231a1b78b7f
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-12-04 14:18:37 -0600

    This commit adds the data structure for what the comment table will look like and how what relationship it will have.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -35,4 +35,24 @@ router.delete('/:id', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const PostData = await Post.update({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!PostData) {
+      res.status(404).json({ message: 'No Post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(PostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
