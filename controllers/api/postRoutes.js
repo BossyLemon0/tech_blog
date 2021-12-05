d50851e41c2eff448e7330cc2fdf26a2f508727d
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-12-05 15:50:32 -0600

    This commit adds changes the database to fit the needs of this project. A quick note, if you delete something that was a require column in sequilze, you must update your database in mysql to fit that need or else you will be stuck wondering why your routes are saying yout not putting in all the right info.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -4,12 +4,11 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newPost = await Post.create({
+    const postData = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     });
-
-    res.status(200).json(newPost);
+    res.status(200).json(postData);
   } catch (err) {
     res.status(400).json(err);
   }
--- public/js/profile.js
+++ public/js/profile.js
@@ -12,7 +12,6 @@ const newFormHandler = async (event) => {
         'Content-Type': 'application/json',
       },
     });
-
     if (response.ok) {
       document.location.replace('/profile');
     } else {
